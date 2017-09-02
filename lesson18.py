c90bcbb72c98a63b62e532130d65a892683822f6
ypypjay <ypypjay@users.noreply.github.com>
2017-09-02 22:51:29 +0800

    Test Adding
    this is a test

---
+++ lesson18.py
@@ -0,0 +1,11 @@
+def find(big):
+    i=100
+    while (i<=big) :
+        whole=str(i)
+        if i == int(whole[0])**3 + int(whole[1])**3 + int(whole[2])**3:
+            print (i,sep=' ')
+        i+=1
+    else:
+        pass
+
+find(999)
